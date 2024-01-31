7cbde58fd3e1ab8ad961235cdc262818a996bc73
Ramana Charan <34278782+rrcharan@users.noreply.github.com>
2024-01-31 18:41:09 +0530

    chroma

--- ragdemo.py
+++ ragdemo.py
@@ -178,7 +178,7 @@ else:
     llm_name = "gpt-3.5-turbo"
 # print(llm_name)
 
-llm = ChatOpenAI(model_name= llm_name, temperature=0, openai_api_key=openai_api_key)
+
 
 # qa_chain = RetrievalQA.from_chain_type(
 #     llm,
@@ -194,6 +194,8 @@ llm = ChatOpenAI(model_name= llm_name, temperature=0, openai_api_key=openai_api_
 
 
 def generate_prompt(context, user_text):
+
+    llm = ChatOpenAI(model_name= llm_name, temperature=0, openai_api_key=openai_api_key)
 
     template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
     {context}
